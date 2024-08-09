2c5a3fa8d548661da3cf81358b93ec73acd4d9e0
dilution0216 <hglad0216@naver.com>
2024-08-09 18:48:56 +0900

    git init

---
+++ src/test/java/com/example/buyme/BuymeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buyme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuymeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
