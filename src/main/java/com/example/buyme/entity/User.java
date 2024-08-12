7cc770ebb93c2b9e42c7917b71e0ac91c00ce9e1
dilution0216 <hglad0216@naver.com>
2024-08-12 18:08:55 +0900

    feat : User, User repo

---
+++ src/main/java/com/example/buyme/entity/User.java
@@ -0,0 +1,36 @@
+package com.example.buyme.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String userName;
+
+    @Column(nullable = false, unique = true)
+    private String userEmail;
+
+    @Column(nullable = false)
+    private String userPassword;
+
+    @Column(nullable = false)
+    private String userPhoneNumber;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private boolean emailVerified = false;
+}
