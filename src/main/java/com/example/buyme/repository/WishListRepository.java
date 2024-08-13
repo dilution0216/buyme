e22df396e45592ef50af834e9df25fbd8ed27a81
dilution0216 <hglad0216@naver.com>
2024-08-14 01:00:05 +0900

    feat : WishList 기능 추가 마이페이지에서 위시리스트와 주문 내역을 조회 및 관리할 수 있음
    제품의 상세 페이지로 이동
    상품의 수량 변경 및 주문
    위시리스트 내 항목의 수정 (삭제 포함)

---
+++ src/main/java/com/example/buyme/repository/WishListRepository.java
@@ -0,0 +1,10 @@
+package com.example.buyme.repository;
+
+import com.example.buyme.entity.WishList;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface WishListRepository extends JpaRepository<WishList, Long> {
+    List<WishList> findByUserUserId(Long userId);
+}
