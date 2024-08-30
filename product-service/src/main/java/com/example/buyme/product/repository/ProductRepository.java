5d38847904ba0dad10056369bced178eef9f47a8
dilution0216 <hglad0216@naver.com>
2024-08-30 18:54:39 +0900

    feat : ProductRepository 예약구매 상품과 일반 상품을 구분하여 조회할 수 있는 메소드를 추가

--- product-service/src/main/java/com/example/buyme/product/repository/ProductRepository.java
+++ product-service/src/main/java/com/example/buyme/product/repository/ProductRepository.java
@@ -3,8 +3,11 @@ package com.example.buyme.product.repository;
 import com.example.buyme.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findById(Long productId);
+    List<Product> findByProductType(String productType);
+
 }
