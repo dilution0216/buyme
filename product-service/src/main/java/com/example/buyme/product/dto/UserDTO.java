c52d1204d21dad130bfb404d81db2395d6cae2e1
dilution0216 <hglad0216@naver.com>
2024-09-03 02:01:48 +0900

    feat : product-service / WishListService 기능 개선 : userId를 기반으로 user-service와 통신하여 사용자 정보를 가져오는 코드 추가 / 환경 설정

---
+++ product-service/src/main/java/com/example/buyme/product/dto/UserDTO.java
@@ -0,0 +1,10 @@
+package com.example.buyme.product.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private Long userId;
+    private String userName;
+    private String userEmail;
+}
--- product-service/src/main/java/com/example/buyme/product/service/WishListService.java
+++ product-service/src/main/java/com/example/buyme/product/service/WishListService.java
@@ -1,9 +1,11 @@
 package com.example.buyme.product.service;
 
+import com.example.buyme.product.dto.UserDTO;
 import com.example.buyme.product.entity.WishList;
 import com.example.buyme.product.repository.WishListRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 
@@ -12,12 +14,16 @@ import java.util.List;
 public class WishListService {
 
     private final WishListRepository wishListRepository;
+    private final WebClient.Builder webClientBuilder;
 
     public List<WishList> getWishlistByUser(Long userId) {
+        // user-service로부터 추가적인 사용자 정보를 가져오는 로직을 추가 가능함
+        // 예: UserDTO user = getUserById(userId);
         return wishListRepository.findByUserId(userId);
     }
 
     public WishList addProductToWishList(WishList wishList) {
+        // user-service로부터 userId에 해당하는 유저 정보를 가져오는 로직 추가 가능함
         return wishListRepository.save(wishList);
     }
 
@@ -27,5 +33,15 @@ public class WishListService {
 
     public WishList updateWishListItem(WishList updatedWishList) {
         return wishListRepository.save(updatedWishList);
+    }
+
+
+    private UserDTO getUserById(Long userId) {
+        return webClientBuilder.build()
+                .get()
+                .uri("http://gateway-service/api/users/" + userId)
+                .retrieve()
+                .bodyToMono(UserDTO.class)
+                .block();
     }
 }
