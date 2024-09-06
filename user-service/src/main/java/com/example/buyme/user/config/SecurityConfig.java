dd94a38ea0220e74d10f564a1b3c7f0707808b92
dilution0216 <hglad0216@naver.com>
2024-09-06 11:21:44 +0900

    docs : 시큐리티 config 해제

--- user-service/src/main/java/com/example/buyme/user/config/SecurityConfig.java
+++ user-service/src/main/java/com/example/buyme/user/config/SecurityConfig.java
@@ -2,11 +2,25 @@ package com.example.buyme.user.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // CSRF 비활성화
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll() // 모든 요청에 대해 인증 없이 접근 허용
+                );
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
--- user-service/src/main/java/com/example/buyme/user/controller/AuthController.java
+++ user-service/src/main/java/com/example/buyme/user/controller/AuthController.java
@@ -1,14 +1,14 @@
 package com.example.buyme.user.controller;
 
 
-import com.example.buyme.user.dto.JwtAuthenticationResponse;
-import com.example.buyme.user.dto.LoginRequest;
+
+
 import com.example.buyme.user.dto.SignupRequest;
 import com.example.buyme.user.entity.User;
 import com.example.buyme.user.exception.DuplicateEmailException;
 import com.example.buyme.user.exception.InvalidEmailFormatException;
 import com.example.buyme.user.exception.InvalidPhoneNumberException;
-import com.example.buyme.user.service.AuthService;
+
 import com.example.buyme.user.service.UserService;
 import com.example.buyme.user.util.ValidationUtil;
 import lombok.RequiredArgsConstructor;
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final UserService userService;
-    private final AuthService authService;
+//    private final AuthService authService;
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@RequestBody SignupRequest signupRequest) {
@@ -45,30 +45,25 @@ public class AuthController {
         user.setUserPassword(signupRequest.getUserPassword());
         user.setUserPhoneNumber(signupRequest.getUserPhoneNumber());
         user.setAddress(signupRequest.getAddress());
+        user.setEmailVerified(true); // 임시로 emailVerified를 true로 설정 ( 원래는 false 로 해야함 )
 
         userService.createUser(user);
 
         log.info("유저 {} 등록이 성공적으로 완료되었습니다", user.getUserEmail());
 
-        return ResponseEntity.ok("사용자가 성공적으로 등록되었습니다. 이메일을 확인해 주세요.");
+        return ResponseEntity.ok("사용자가 성공적으로 등록되었습니다.");
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
-        JwtAuthenticationResponse response = authService.login(loginRequest);
-        return ResponseEntity.ok(response);
-    }
-
-    @PostMapping("/logout")
-    public ResponseEntity<?> logoutUser(@RequestHeader("Authorization") String token) {
-        authService.logout(token);
-        return ResponseEntity.ok("로그아웃되었습니다.");
-    }
-
-//    @PutMapping("/logout-all")
-//    public ResponseEntity<?> logoutFromAllDevices(@RequestBody String email) {
-//        User user = userService.findUserByEmail(email);
-//        authService.logoutFromAllDevices(user);
-//        return ResponseEntity.ok("모든 기기에서 로그아웃되었습니다.");
+//    @PostMapping("/login")
+//    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
+//        JwtAuthenticationResponse response = authService.login(loginRequest);
+//        return ResponseEntity.ok(response);
 //    }
+//
+//    @PostMapping("/logout")
+//    public ResponseEntity<?> logoutUser(@RequestHeader("Authorization") String token) {
+//        authService.logout(token);
+//        return ResponseEntity.ok("로그아웃되었습니다.");
+//    }
+
 }
